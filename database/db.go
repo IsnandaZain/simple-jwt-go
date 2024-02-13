847f988dda0db4e6df706c52a7b5c2bf6ce4a5b1
isnandazain <isnanda.muhammad@work.bri.co.id>
2024-02-13 11:52:24 +0700

    first commit

---
+++ database/db.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"go-jwt/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "10051998"
+	dbPort   = "5432"
+	dbname   = "simple-api"
+	db       *gorm.DB
+	err      error
+)
+
+func StartDB() {
+	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, dbPort)
+	dsn := config
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("error connecting to database;", err)
+	}
+
+	log.Println("sukses koneksi ke database")
+	db.Debug().AutoMigrate(
+		models.User{},
+		models.Product{},
+	)
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
